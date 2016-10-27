7f03e2f740c43074e85c4e0a145fe2f4f2fe66ed
leunamme <leunamme@MyAlien.home>
2016-10-27 02:48:28 -0400

    hyy

--- great123/src/great123/greatMain.java
+++ great123/src/great123/greatMain.java
@@ -4,7 +4,7 @@ public class greatMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("fetch123");
+System.out.println("fetch1234");
 	}
 
 }
